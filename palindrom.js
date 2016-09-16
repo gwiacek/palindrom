37b4f7a7d05f605b1ccf0fc4c95deabb2c84eeca
Grzesiek <g.wiacek@yahoo.com>
2016-09-16 14:00:30 +0200

    Update palindrom.js

--- palindrom.js
+++ palindrom.js
@@ -1,22 +1,13 @@
-function palindrom(wyraz) {
-
-    var wyraz = prompt("Wpisz słowo do sprawdzenia")
-    var sprawdz = true;
-    var dlugosc = wyraz.length;
-
-    if (dlugosc === 0)
-        return "Nic nie wpisałeś!";
-
-    else {
-        for (var i = 0; i < dlugosc; i++) {
-            if (wyraz[i] != wyraz[(dlugosc - i - 1)])
-                sprawdz = false;
-        }
-
-        if (sprawdz === true)
-           return`Słowo ${wyraz} jest palindromem`;
-        else
-            return`Słowo ${wyraz} nie jest palindromem`;
-    }
+function palindrom(sprSlowo){
+  sprSlowo = prompt("wpisz slowo do sprawdzenia");
+  var dlugosc = sprSlowo.length;
+  
+  for( var i = 0 ; i < dlugosc; i++){
+    if (sprSlowo[i] === sprSlowo[(dlugosc - i - 1)])
+      {
+        return ("slowo jest palindromem");     
+  }
+     {return ("slowo nie jest palindromem");} 
+}
 }
 console.log(palindrom());
